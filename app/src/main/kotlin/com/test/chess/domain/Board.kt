0a1f36d6c3bc48ff694152245f7f54624c63846a
Pankaj Sharma <pankaj@alefeducation.com>
2021-01-15 09:15:17 +0400

    Refactor remove unused code from Board

--- app/src/main/kotlin/com/test/chess/domain/Board.kt
+++ app/src/main/kotlin/com/test/chess/domain/Board.kt
@@ -1,16 +1,3 @@
 package com.test.chess.domain
 
-class Board {
-    fun cellAbove(currentPosition: String): String? {
-
-        TODO("Not yet implemented")
-    }
-
-    fun cellAboveToLeft(currentPosition: String): String? {
-        TODO("Not yet implemented")
-    }
-
-    fun cellAboveToRight(currentPosition: String): String? {
-        TODO("Not yet implemented")
-    }
-}
+class Board {}
